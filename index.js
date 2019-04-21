9ef2fd09610309f58885cc4773370e156d55a687
Lord1Luciani <>
2019-04-21 16:30:08 +0200

    r

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const telegraf = require('telegraf')
+require('dotenv').config()
+const Telegraf = require('telegraf')
+const axios = require('axios')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.launch()
