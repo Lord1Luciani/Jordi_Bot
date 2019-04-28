4ff7b0b6d1556ec630c4e667fd6fa7d7481a4484
Lord1Luciani <>
2019-04-28 02:13:17 +0200

    ,

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.command('qwer', (ctx) => {
 bot.hears(/^!rate($|\s.*)/, async (ctx) => {
   if (ctx.chat.id === 686968130) {
     SPEED_RATE = await setRatio(ctx)
-    ctx.reply(`Rate is ${Math.ceil(SPEED_RATE / 1000)}`)
+    ctx.reply(`Rate is ${Math.ceil(SPEED_RATE / 1000)} sec`)
       .then((msg) => {
         setTimeout(() => {
           telegram.deleteMessage(ctx.chat.id, msg.message_id - 1)
